4a69c7493e9de98fc36dce54995052d70eb0e813
Nate Cottle <cottle.nate@gmail.com>
2018-06-27 17:54:22 -0700

    change palindrome? method to no longer use reverse

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,11 @@
+require 'pry'
 def palindrome? (word)
   word_array = word.downcase.split(" ").join("").split("")
-  reversed_word = word_array.reverse
+  reversed_word = []
+  word_array.each do |letter|
+    reversed_word.unshift(letter)
+  end
+  binding.pry
   if word_array == reversed_word
     true
   else
